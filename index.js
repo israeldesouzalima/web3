08c3e6caf12201348dde496103d45f580f030154
Israel de Souza <israellimas@hotmail.com>
2025-08-08 01:17:39 -0300

    resolução de exercícios, exceto questão dissertativa

---
+++ index.js
@@ -0,0 +1,115 @@
+// Exercicio 01
+
+function saudacao(name, lastName) {
+  console.log(`Olá ${name} ${lastName}, eu sou uma função!!`);
+}
+
+saudacao("João", "da Silva");
+
+const saudacaoArrow = (name, lastName) =>
+  console.log(`Olá ${name} ${lastName}, eu sou uma função!!`);
+
+saudacaoArrow("João", "Santos");
+
+// Exercicio 02
+
+function descobreDiametro(raio) {
+  return 2 * raio;
+}
+
+const diametro01 = descobreDiametro(4);
+
+console.log(`O diametro, cujo raio seja igual a 4, é: `, diametro01);
+
+const descobreDiametroArrow = (raio) => {
+  return 2 * raio;
+};
+
+const diametro02 = descobreDiametroArrow(12);
+
+console.log(`O diametro, cujo raio seja igual a 12, é: `, diametro02);
+
+// Exercicio 03
+// Dados
+let carrinho = [
+  { nome: "abacaxi", preco: "2.00" },
+  { nome: "detergente", preco: "2.50" },
+  { nome: "bolacha", preco: "3.80" },
+];
+
+let itens = ["abacaxi", "banana", "maçã", "laranja", "limão"];
+
+let palavroes = [
+  "Inconstitucionalíssimo",
+  "Otorrinolaringologista",
+  "Pneumoultramicroscopicossilicovulcanoconiose",
+  "Oftalmotorrinolaringologista",
+];
+
+// Item 01. Antes:
+
+carrinho.forEach(function (produto) {
+  imprimeProduto(produto.nome, produto.preco);
+});
+
+function imprimeProduto(nome, preco) {
+  console.log("Produto: " + nome + " | " + "Preço: " + preco);
+}
+
+// Apos:
+
+carrinho.forEach((produto, indice) => {
+  console.log(`Produto ${indice}: ${produto.nome} | Preço: ${produto.preco}`);
+});
+
+// Item 02. Antes:
+
+itens.forEach(function (item) {
+  console.log(item);
+});
+
+// Após:
+item.forEach((item) => console.log(item));
+
+// Item 03. Toda função declarada no escopo global possui o objeto `window` como valor do `this`. Por que acontece isso?
+/**
+ *
+ */
+
+// Item 04. Antes:
+
+let tamanhos = palavroes.map(function (palavrao) {
+  return palavrao.length;
+});
+console.log(tamanhos); // [ 22, 22, 44, 28 ]
+
+// Após:
+palavroes.map((palavrao) => console.log(palavrao.length));
+
+// Item 05. Antes:
+
+var equipe = {
+  nome: "Valentes da Glória",
+  membros: ["Luciano", "Maria", "Virgínia", "Daniela"],
+  imprimeNomes: function () {
+    var that = this;
+    this.membros.forEach(function (membro) {
+      console.log(membro + " é da equipe " + that.nome);
+    });
+  },
+};
+
+equipe.imprimeNomes();
+
+// Depois:
+
+const equipe2 = {
+  nome: "Valentes da Glória",
+  membros: ["Luciano", "Maria", "Virgínia", "Daniela"],
+
+  imprimeNomes() {
+    this.membros.forEach((membro) => {
+      console.log(`${membro} é da equipe ${this.nome}`);
+    });
+  },
+};
