d30eff6c05cb4c61a62b72e885fcbe8f61750c82
Israel de Souza <israellimas@hotmail.com>
2025-08-27 03:36:56 -0300

    renderização incompleta, página 14

--- 02-react-native/App.js
+++ 02-react-native/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
 import * as Location from "expo-location";
 import { Text, View, StyleSheet } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import MapView from "react-native-maps";
 
 export default class App extends Component {
   state = {
-    places: [{ errorMessage: null, loaded: false, location: null }],
+    errorMessage: null,
+    location: null,
+    bobinho: {
+      lgn: null,
+    },
+    locationAddress: null,
+    street: null,
+    subregion: null,
+    region: null,
+    country: null,
+    postalCode: null,
+    district: null,
   };
 
   componentDidMount() {
@@ -15,25 +28,81 @@ export default class App extends Component {
     let { status } = await Location.requestForegroundPermissionsAsync();
 
     if (status !== "granted") {
-      this.setState({ errorMessage: "Permissão ao GPS negada!", loaded: true });
+      this.setState({ errorMessage: "Permissão de localização negadaaaaa!" });
     } else {
-      let location = await Location.getCurrentPositionAsync({enableHighAccuracy: true});
-      this.setState({ location, loaded: true, errorMessage: null });
+      let location = await Location.getCurrentPositionAsync({});
+      this.setState({
+        errorMessage: "Permissão de localização concedida!",
+        location: location,
+      });
+
+      const { latitude, longitude } = location.coords;
+
+      this.setState({
+        bobinho: {
+          ...this.state.bobinho,
+          lgn: longitude,
+        },
+      });
+
+      let locationAddress = await Location.reverseGeocodeAsync({
+        latitude: latitude,
+        longitude: longitude,
+      });
+
+      this.setState({ locationAddress });
+
+      const address = locationAddress;
+
+      const [{ street, subregion, region, country, postalCode, district }] =
+        address;
+
+      this.setState({
+        street,
+        subregion,
+        region,
+        country,
+        postalCode,
+        district,
+      });
     }
   };
 
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.myText}> Hello, friends!! </Text>
-        <Text style={styles.myText}> Hello, friends!! </Text>
-        <Text style={styles.myText}> Hello, friends!! </Text>
-        <Text style={styles.myText}> Hello, friends!! </Text>
-        <Text style={styles.myText}> Hello, friends!! </Text>
-        <Text style={styles.myText}> Hello, friends!! </Text>
-        <Text style={styles.myText}> Hello, friends!! </Text>
-      </View>
-    );
+    if (this.state.errorMessage === "Permissão de localização negadaaaaa!") {
+      return (
+        <View style={styles.container}>
+          <Text>{JSON.stringify(this.state.errorMessage)}</Text>
+        </View>
+      );
+    } else if (
+      this.state.location &&
+      this.state.location.coords &&
+      typeof this.state.location.coords.latitude === "number" &&
+      typeof this.state.location.coords.longitude === "number"
+    ) {
+      return (
+        <View style={styles.container}>
+          <MapView
+            ref={(map) => (this.mapStyle = map)}
+            region={{
+              latitude: this.state.location.coords.latitude,
+              longitude: this.state.location.coords.longitude,
+              latitudeDelta: 0.0142,
+              longitudeDelta: 0.0231,
+            }}
+            style={styles.mapStyle}
+            showsBuildings={false}
+          />
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.container}>
+          <Text> Espere...</Text>
+        </View>
+      );
+    }
   }
 }
 
@@ -45,5 +114,36 @@ const styles = StyleSheet.create({
   },
   myText: {
     fontSize: 23,
+    textAlign: "center",
+  },
+  mapStyle: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
   },
 });
+
+// return (
+//   <View style={styles.container}>
+//     <Text style={styles.myText}> Onde eu estou?? </Text>
+//     {this.state.location && (
+//       <Text style={styles.myText}>
+//         Contemple o seu endereço por completo: {"\n"}
+//         {this.state.street}
+//         {"\n"}
+//         {this.state.district}
+//         {"\n"}
+//         {this.state.subregion}
+//         {"\n"}
+//         {this.state.postalCode}
+//         {"\n"}
+//         {this.state.region}
+//         {"\n"}
+//         {this.state.country}
+//         {"\n"}
+//       </Text>
+//     )}
+//   </View>
+// );
