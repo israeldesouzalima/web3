2c2dd6bc0509f8eff9a508fdc58a03d4c746a428
Israel de Souza <israellimas@hotmail.com>
2025-08-26 23:21:48 -0300

    capturando a geolocalização

--- 02-react-native/App.js
+++ 02-react-native/App.js
@@ -1,16 +1,40 @@
-import React, { useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
+import React, { Component } from "react";
+import * as Location from "expo-location";
+import { Text, View, StyleSheet } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.myText}> Hello, App </Text>
-    </View>
-  );
+export default class App extends Component {
+  state = {
+    places: [{ errorMessage: null, loaded: false, location: null }],
+  };
+
+  componentDidMount() {
+    this._getLocation();
+  }
+
+  _getLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+
+    if (status !== "granted") {
+      this.setState({ errorMessage: "Permissão ao GPS negada!", loaded: true });
+    } else {
+      let location = await Location.getCurrentPositionAsync({enableHighAccuracy: true});
+      this.setState({ location, loaded: true, errorMessage: null });
+    }
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.myText}> Hello, friends!! </Text>
+        <Text style={styles.myText}> Hello, friends!! </Text>
+        <Text style={styles.myText}> Hello, friends!! </Text>
+        <Text style={styles.myText}> Hello, friends!! </Text>
+        <Text style={styles.myText}> Hello, friends!! </Text>
+        <Text style={styles.myText}> Hello, friends!! </Text>
+        <Text style={styles.myText}> Hello, friends!! </Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -20,6 +44,6 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   myText: {
-    fontSize: 22,
+    fontSize: 23,
   },
 });
